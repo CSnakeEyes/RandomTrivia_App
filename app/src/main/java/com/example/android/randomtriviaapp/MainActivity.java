3252dfa8f653f8b7a491589ebafd09e500a99d2e
Cristian Ayub <crisaych@gmail.com>
2018-10-13 17:25:13 -0600

    Added more answers and added some logic to identify which is the correct answer. Also added a reset option by clicking the question. Finally, a displaying icon was added so that answers could be hidden and shown.

--- app/src/main/java/com/example/android/randomtriviaapp/MainActivity.java
+++ app/src/main/java/com/example/android/randomtriviaapp/MainActivity.java
@@ -14,16 +14,54 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcardQ1_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                findViewById(R.id.flashcardQ1_tv).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcardA1_tv).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcardA1_tv).setBackgroundColor(getResources().getColor(R.color.lightOrange));
+                findViewById(R.id.flashcardA2_tv).setBackgroundColor(getResources().getColor(R.color.lightOrange));
+                findViewById(R.id.flashcardA3_tv).setBackgroundColor(getResources().getColor(R.color.lightOrange));
             }
         });
 
         findViewById(R.id.flashcardA1_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                findViewById(R.id.flashcardA1_tv).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+            }
+        });
+
+        findViewById(R.id.flashcardA2_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.flashcardA1_tv).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+                findViewById(R.id.flashcardA2_tv).setBackgroundColor(getResources().getColor(R.color.lightRed));
+            }
+        });
+
+        findViewById(R.id.flashcardA3_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.flashcardA1_tv).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+                findViewById(R.id.flashcardA3_tv).setBackgroundColor(getResources().getColor(R.color.lightRed));
+            }
+        });
+
+        findViewById(R.id.toggle_choices_invisible).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.toggle_choices_invisible).setVisibility(View.INVISIBLE);
+                findViewById(R.id.toggle_choices_visible).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcardA1_tv).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcardQ1_tv).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcardA2_tv).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcardA3_tv).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.toggle_choices_visible).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.toggle_choices_invisible).setVisibility(View.VISIBLE);
+                findViewById(R.id.toggle_choices_visible).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcardA1_tv).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcardA2_tv).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcardA3_tv).setVisibility(View.VISIBLE);
             }
         });
     }
