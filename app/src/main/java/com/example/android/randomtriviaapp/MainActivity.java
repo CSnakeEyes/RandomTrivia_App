5c4332ffb4145b6092a4266a9424b22ff67e3cd4
Cristian Ayub <crisaych@gmail.com>
2018-11-17 02:54:21 -0700

    Added new features to the app, such as: delete question and have different flash cards in a database made with Room. However, there is a bug with the cancel button.

--- app/src/main/java/com/example/android/randomtriviaapp/MainActivity.java
+++ app/src/main/java/com/example/android/randomtriviaapp/MainActivity.java
@@ -8,21 +8,29 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int NEW_CARD_REQUEST_CODE = 100;
+    private final int EDIT_CARD_REQUEST_CODE = 200;
 
     FlashcardDatabase flashcardDatabase;        // Creation of database using Room Library
     List<Flashcard> allFlashcards;              // List of created flashcards
     int currentCardDisplayedIndex = 0;          // Current flashcard displayed
+    Flashcard cardToEdit;
+
+    public int getRandomNumber(int minNumber, int maxNumber) {
+        Random rand = new Random();
+        return rand.nextInt((maxNumber - minNumber) + 1) + minNumber;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        flashcardDatabase = new FlashcardDatabase(this);
         allFlashcards = flashcardDatabase.getAllCards();
 
         if (allFlashcards != null && allFlashcards.size() > 0) {
@@ -104,13 +112,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, NewCardActivity.class);
-                MainActivity.this.startActivityForResult(intent, 100);
+                MainActivity.this.startActivityForResult(intent, NEW_CARD_REQUEST_CODE);
             }
         });
 
         findViewById(R.id.edit_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                cardToEdit = allFlashcards.get(currentCardDisplayedIndex);
+                System.out.println("Hello this: " + cardToEdit.getQuestion());
+
                 String question = ((TextView) findViewById(R.id.flashcardQ1_tv)).getText().toString();
                 String correctAnswer = ((TextView) findViewById(R.id.flashcardA_tv)).getText().toString();
                 String wrongAnswerOne = ((TextView) findViewById(R.id.flashcardA2_tv)).getText().toString();
@@ -121,19 +132,19 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("answer", correctAnswer);
                 intent.putExtra("wrongOne", wrongAnswerOne);
                 intent.putExtra("wrongTwo", wrongAnswerTwo);
-                MainActivity.this.startActivityForResult(intent, NEW_CARD_REQUEST_CODE);
+                MainActivity.this.startActivityForResult(intent, EDIT_CARD_REQUEST_CODE);
             }
         });
 
         findViewById(R.id.next_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentCardDisplayedIndex++;
+                currentCardDisplayedIndex = getRandomNumber(0, allFlashcards.size() - 1);
 
                 // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
-                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
-                    currentCardDisplayedIndex = 0;
-                }
+//                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+//                    currentCardDisplayedIndex = 0;
+//                }
 
                 findViewById(R.id.flashcardA_tv).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcardQ1_tv).setVisibility(View.VISIBLE);
@@ -182,9 +193,17 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.flashcardA1_tv)).setText(correctAnswer);
             ((TextView) findViewById(R.id.flashcardA2_tv)).setText(wrongAnswerOne);
             ((TextView) findViewById(R.id.flashcardA3_tv)).setText(wrongAnswerTwo);
+
+            flashcardDatabase.insertCard(new Flashcard(question, correctAnswer, wrongAnswerOne, wrongAnswerTwo));
+        } else if(requestCode == EDIT_CARD_REQUEST_CODE && resultCode == RESULT_OK) {
+            cardToEdit.setQuestion(question);
+            cardToEdit.setAnswer(correctAnswer);
+            cardToEdit.setWrongAnswer1(wrongAnswerOne);
+            cardToEdit.setWrongAnswer2(wrongAnswerTwo);
+
+            flashcardDatabase.updateCard(cardToEdit);
         }
 
-        flashcardDatabase.insertCard(new Flashcard(question, correctAnswer, wrongAnswerOne, wrongAnswerTwo));
         allFlashcards = flashcardDatabase.getAllCards();
 
         Snackbar.make(findViewById(R.id.flashcardQ1_tv),
